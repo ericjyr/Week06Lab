6e6d6da025cdee9c2afd6b5ad35d8929451b1bde
Eric Rebadona <ericjyr@gmail.com>
2023-01-19 13:15:41 -0700

    Completed and tested Week 6 Lab

--- src/java/servlets/ShoppingListServlet.java
+++ src/java/servlets/ShoppingListServlet.java
@@ -6,6 +6,7 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -26,20 +27,28 @@ public class ShoppingListServlet extends HttpServlet {
             throws ServletException, IOException {
         
         HttpSession session = request.getSession(false);
+        String username = null;
         
-        if(session != null){
-        String username = (String)session.getAttribute("username");
-        
-        request.setAttribute("username", username);
-        getServletContext().getRequestDispatcher("/WEB-INF/ShoppingList.jsp" )
-            .forward(request, response); 
+        if (session != null){
+            username = (String)session.getAttribute("username");
         }
-   
-            getServletContext().getRequestDispatcher("/WEB-INF/register.jsp" )
-                .forward(request, response);
-
-
-
+        
+        if(username != null){
+            String action = request.getParameter("action");
+            
+            if(action.equals("logout")){ 
+                session.invalidate();
+            }
+            else {
+                request.setAttribute("username", username);
+                getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp" )
+                    .forward(request, response); 
+                return;
+            }
+        }
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/register.jsp" )
+            .forward(request, response);
     }
 
 
@@ -47,24 +56,53 @@ public class ShoppingListServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-        String action = (String)request.getAttribute("action");
+        //gets value of hidden input tag named action
+        String action = request.getParameter("action");
+        
+        //only instantiates if object exists otherwise null
+        HttpSession session = request.getSession(false);
         
         switch (action){
             case "register":
-                HttpSession session = request.getSession();
+                //creation of Session, String, and ArrayList object
+                session = request.getSession();
                 String username = request.getParameter("username");
-                session.setAttribute("username", username);
-                response.sendRedirect("/Week6Lab_ShoppingList/ShoppingList");
-                break;
-            case "add":
+                ArrayList<String> items = new ArrayList<>();
                 
-                break;                
+                //storing String and ArrayList object within session
+                session.setAttribute("username", username);
+                session.setAttribute("items", items);
+                break;
+                
+            case "add":
+                //gets value of input tag with name item
+                String item = request.getParameter("item");
+                //gets ArrayList stored within session
+                items = (ArrayList<String>)session.getAttribute("items"); 
+                //add item to array and setting it once again in the session object
+                items.add(item);
+                session.setAttribute("items", items);
+                break;  
+                
             case "delete":
-                break;                    
+                items = (ArrayList<String>)session.getAttribute("items");
+                
+                for (int i = 0; i < items.size(); i++) {
+                    String elem = items.get(i);
+                    
+                    //if radio button is checked, value of tag is returned otherwise null
+                    String checked = request.getParameter(elem);
+                    
+                    if(elem.equals(checked)) {
+                        items.remove(i--);// decrement i to compensate
+                    }
+                }
+                session.setAttribute("items", items);
+                break;   
         }
-
         
-        
+        getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp" )
+                    .forward(request, response); 
     }
 
 
--- web/WEB-INF/shoppingList.jsp
+++ web/WEB-INF/shoppingList.jsp
@@ -4,6 +4,7 @@
     Author     : ericr
 --%>
 
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -24,12 +25,23 @@
             <input type="submit" value="Add Item">
             <input type="hidden" name="action" value="add">
         </form>
+        
         <form action="" method="POST">
             <ul>
-            
+                <c:forEach var="item" items="${items}">
+                    <li> 
+                        <input type="radio" name="${item}" value="${item}">${item}
+                    </li>
+                </c:forEach>
             </ul>
             <input type="submit" value="Delete">
             <input type="hidden" name="action" value="delete">
         </form>
+        
+        <style>
+            li {             
+            list-style-type: none;
+            }
+        </style>
     </body>
 </html>
